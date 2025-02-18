4b9f3650775867e1f7a1f565a873cd29b6cda9b5
Samet Aydinhan <samet.aydinhan879@gmail.com>
2025-02-18 23:11:14 +0300

    feat:enhance database connection handling and add user registration endpoint

--- movie-app-backend/index.js
+++ movie-app-backend/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./db");
 const app = express();
 const port = 3001;
 
@@ -6,9 +8,19 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // routes
 app.post('/register', (req, res) => {
-    
+    const query = "INSERT INTO users (username, password) VALUES (?, ?)";
+
+    db.query(query, [req.body.username, req.body.password], (err, rows) => {
+        if(err){
+            console.log(err);
+        }
+        res.send("User created");
+    });
 });
 
 app.listen(port, () => {
