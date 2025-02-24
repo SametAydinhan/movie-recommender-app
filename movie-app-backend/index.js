ae3c8a281f8fe38d65d7e9871759ec74307eaa0b
Samet Aydinhan <samet.aydinhan879@gmail.com>
2025-02-24 13:41:11 +0300

    feat: add user registration with username check and password hashing

--- movie-app-backend/index.js
+++ movie-app-backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt"); //npm install bcrypt https://github.com/kelektiv/node.bcrypt.js
 const db = require("./db");
 const app = express();
 const port = 3001;
@@ -14,13 +15,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // routes
 app.post('/register', (req, res) => {
     const query = "INSERT INTO users (username, password) VALUES (?, ?)";
+    const query2 = "SELECT * FROM users WHERE username = ?";
 
-    db.query(query, [req.body.username, req.body.password], (err, rows) => {
-        if(err){
+    db.query(query2, [req.body.username], async (err, rows) => {
+      if (err) {
+        console.log(err);
+      }
+      if (rows.length > 0) {
+        return res.status(400).send("User already exists");
+      }
+      if(rows.length === 0){
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        db.query(query, [req.body.username, hashedPassword], (err, rows) => {
+          if (err) {
             console.log(err);
-        }
-        res.send("User created");
-    });
+          }
+          res.send("User created");
+        });
+      }
+  });
 });
 
 app.listen(port, () => {
