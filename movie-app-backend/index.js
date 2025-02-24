6c050a91031985f41a1ad9ce33d3c300e0faa20e
Samet Aydinhan <samet.aydinhan879@gmail.com>
2025-02-24 17:02:22 +0300

    feat: enhance user registration with email validation and CORS support

--- movie-app-backend/index.js
+++ movie-app-backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const bcrypt = require("bcrypt"); //npm install bcrypt https://github.com/kelektiv/node.bcrypt.js
 const db = require("./db");
 const app = express();
@@ -12,29 +13,52 @@ app.get("/", (req, res) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// routes
-app.post('/register', (req, res) => {
-    const query = "INSERT INTO users (username, password) VALUES (?, ?)";
-    const query2 = "SELECT * FROM users WHERE username = ?";
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 
-    db.query(query2, [req.body.username], async (err, rows) => {
-      if (err) {
-        console.log(err);
-      }
-      if (rows.length > 0) {
-        return res.status(400).send("User already exists");
-      }
-      if(rows.length === 0){
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        db.query(query, [req.body.username, hashedPassword], (err, rows) => {
-          if (err) {
-            console.log(err);
-          }
-          res.send("User created");
-        });
-      }
+// routes
+app.post("/register", async (req, res) => {
+  console.log("Gelen veriler:", req.body);
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).send("Lütfen tüm alanları doldurun.");
+  }
+
+  const checkUserQuery = "SELECT * FROM users WHERE username = ? OR email = ?";
+  const insertUserQuery =
+    "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+
+  db.query(checkUserQuery, [username, email], async (err, rows) => {
+    if (err) {
+      console.error("Veritabanı hatası:", err);
+      return res.status(500).send("Sunucu hatası.");
+    }
+
+    if (rows.length > 0) {
+      return res
+        .status(400)
+        .send("Bu kullanıcı adı veya email zaten kullanılıyor.");
+    }
+
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      db.query(insertUserQuery, [username, email, hashedPassword], (err) => {
+        if (err) {
+          console.error("Kullanıcı eklenirken hata oluştu:", err);
+          return res.status(500).send("Kayıt işlemi başarısız.");
+        }
+        res.send("Kullanıcı başarıyla oluşturuldu.");
+      });
+    } catch (hashError) {
+      console.error("Şifre hashleme hatası:", hashError);
+      res.status(500).send("Şifre işlenirken hata oluştu.");
+    }
   });
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
