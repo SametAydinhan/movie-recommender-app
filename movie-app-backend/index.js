5b846265de2d85e11d9fd72581131d0a49759a7c
Samet Aydinhan <samet.aydinhan879@gmail.com>
2025-02-25 21:17:52 +0300

    feat: implement user authentication with Passport.js and session management

--- movie-app-backend/index.js
+++ movie-app-backend/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const bcrypt = require("bcrypt"); //npm install bcrypt https://github.com/kelektiv/node.bcrypt.js
+const passport = require("passport");
+const expressSession = require("express-session");
+const bcrypt = require("bcrypt"); 
+const cookieParser = require("cookie-parser");
 const db = require("./db");
 const app = express();
 const port = 3001;
@@ -12,11 +15,18 @@ app.get("/", (req, res) => {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSession({secret: 'cairocoders-ednalan', resave: false, saveUninitialized: false}));
 
 app.use(cors({
   origin: "http://localhost:3000",
   credentials: true
 }));
+
+app.use(cookieParser("cairocoders-ednalan"));
+
+app.use(passport.initialize());
+app.use(passport.session());
+require("./passportConfig")(passport);
 
 // routes
 app.post("/register", async (req, res) => {
@@ -57,6 +67,34 @@ app.post("/register", async (req, res) => {
       res.status(500).send("Şifre işlenirken hata oluştu.");
     }
   });
+});
+
+app.post("/login", async (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      console.log(err);
+      return next(err);
+    }
+    if (!user) {
+      return res.status(400).send("Kullanıcı adı veya şifre hatalı.");
+    }
+    req.login(user, (err) => {
+      if (err) {
+        console.log(err);
+        return next(err);
+      }
+      res.send("Giriş başarılı.");
+      console.log(user);
+    });
+  })(req, res, next);
+});
+
+
+app.get("/getUser", (req, res) => {
+  if (req.isAuthenticated()) {
+    return res.send(req.user);
+  }
+  res.status(401).send("Giriş yapmamış kullanıcılar bu alana erişemez.");
 });
 
 
